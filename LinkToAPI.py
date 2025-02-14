f9f43eeaed213e7b9c13516ea71770004839dd55
X.Huang <ft1120334@126.com>
2025-02-14 14:17:30 +0800

    Update LinkToAPI.py

--- LinkToAPI.py
+++ LinkToAPI.py
@@ -4,3 +4,11 @@ from binance.client import Client
 api_key = '你的API Key'  
 api_secret = '你的Secret Key'  
 client = Client(api_key, api_secret)  
+
+#实时数据监听#
+//使用python-binance的Websocket接口监听实时数据：
+cancel = client.cancel_order(  
+    symbol='BTCUSDT',  
+    orderId=order['orderId']  
+)  
+print(cancel)  
